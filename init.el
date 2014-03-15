affef0d546fdd668aedc4efdd7aeb977fd872906
yang song <song@yangs-MacBook-Pro.local>
2014-03-15 00:54:11 -0400

    my emacs24 configure files in .emacs.d/ modified init.el based on starter-kit

---
+++ init.el
@@ -0,0 +1,51 @@
+;;; init.el --- Where all the magic begins
+;;
+;; Part of the Emacs Starter Kit
+;;
+;; This is the first thing to get loaded.
+;;
+
+;; load Org-mode from source when the ORG_HOME environment variable is set
+(when (getenv "ORG_HOME")
+  (let ((org-lisp-dir (expand-file-name "lisp" (getenv "ORG_HOME"))))
+    (when (file-directory-p org-lisp-dir)
+      (add-to-list 'load-path org-lisp-dir)
+      (require 'org))))
+
+;; load the starter kit from the `after-init-hook' so all packages are loaded
+(add-hook 'after-init-hook
+ `(lambda ()
+    ;; remember this directory
+    (setq starter-kit-dir
+          ,(file-name-directory (or load-file-name (buffer-file-name))))
+    ;; only load org-mode later if we didn't load it just now
+    ,(unless (and (getenv "ORG_HOME")
+                  (file-directory-p (expand-file-name "lisp"
+                                                      (getenv "ORG_HOME"))))
+       '(require 'org))
+    ;; load up the starter kit
+    (org-babel-load-file (expand-file-name "starter-kit.org" starter-kit-dir))))
+
+
+(require 'package)
+(add-to-list 'package-archives 
+    '("marmalade" .
+      "http://marmalade-repo.org/packages/"))
+(package-initialize)
+
+;; Use monokai-theme
+(load-theme 'monokai t)
+
+;; Display line numbers
+(global-linum-mode t)
+
+;; Disable generating #file#
+(setq auto-save-default nil)
+
+;; Disable backup file
+(setq backup-inhibited t)
+
+;; Command prompt when using M-x
+(icomplete-mode 1)
+
+;;; init.el ends here
